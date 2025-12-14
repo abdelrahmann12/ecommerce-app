07c006f12ccbce86131d98d7f14708353bfaa8f8
abdelrahmann12 <al3sar.2016@gmail.com>
2025-12-14 03:42:56 +0200

    solve token error for deploy

--- src/getUserToken.ts
+++ src/getUserToken.ts
@@ -3,9 +3,10 @@ import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
 export async function GetTokenValue(){
+   const TokenSession  = (process.env.NODE_ENV === "production" ? '__Secure-next-auth.callback-url'  : 'next-auth.session-token' )
    const cookiesData =  await cookies()
    console.log("All cookies:", cookiesData.getAll());
-   const encryotToken=  cookiesData.get('next-auth.session-token')?.value
+   const encryotToken=  cookiesData.get(TokenSession)?.value
    
   const token = await decode({token:encryotToken , secret:process.env.AUTH_SECRET!})
   console.log("token is " , token)
